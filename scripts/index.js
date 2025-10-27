8c14450b1c0364a2d8d2f58ce123dc4ee14b0705
daniel-fernandez25 <danielfb0554@gmail.com>
2025-10-26 20:55:42 -0600

    add: - logic to close and open profile modal - logic to fill the profile name and description input fields - logic to edit the name and description from profile modal

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,69 @@ initialCards = [
   },
 ];
 
-initialCards.forEach(function (card) {
-  console.log(card.name);
-});
+// initialCards.forEach(function (card) {
+//   console.log(card.name);
+// });
+
+// selecting edit profile modal and buttons
+const profilePageSection = document.querySelector(".profile.page__section"); //profile section page
+const editProfileButton = profilePageSection.querySelector(
+  ".profile__edit-button"
+); // edit profile button
+const editProfileModal = document.querySelector("#edit-popup"); // modal itself
+const closeProfileSection = editProfileModal.querySelector(".popup__close"); // close (profile modal) button
+const nameField = editProfileModal.querySelector(".popup__input_type_name");
+const descriptionField = editProfileModal.querySelector(
+  ".popup__input_type_description"
+);
+const editProfileForm = editProfileModal.querySelector("#edit-profile-form");
+// selecting the profile info
+const profileName = profilePageSection.querySelector(".profile__title");
+const profileDescription = profilePageSection.querySelector(
+  ".profile__description"
+);
+
+//anonymus function that calls the open modal function when the click happens
+editProfileButton.addEventListener("click", () =>
+  handleOpenEditModal(editProfileModal)
+);
+
+closeProfileSection.addEventListener("click", () =>
+  closeModal(editProfileModal)
+);
+
+function handleOpenEditModal(modal) {
+  openModal(modal);
+  fillProfileForm();
+}
+
+// opens modal
+function openModal(modal) {
+  modal.classList.add("popup_is-opened"); // no retrun statement as no value needs to be returned
+  fillProfileForm();
+}
+// closes modal
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  // matches the initial name in the name field w the one in the profile
+  nameField.value = profileName.textContent;
+  descriptionField.value = profileDescription.textContent;
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  const nameInput = editProfileForm.querySelector(".popup__input_type_name");
+  const descriptionInput = editProfileForm.querySelector(
+    ".popup__input_type_description"
+  );
+  profileName.textContent = nameInput.value;
+  // console.log(`in profile: ${profileName.value}; input: ${nameInput.value}`);
+  profileDescription.textContent = descriptionInput.value;
+  console.log(`in profile: ${profileDescription.textContent}`);
+  closeModal(editProfileModal);
+}
+
+editProfileForm.addEventListener("submit", handleProfileFormSubmit);
