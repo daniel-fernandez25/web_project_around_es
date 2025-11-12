23d6f8b2144ac2bb9ae119e084c95f4aacedd624
daniel-fernandez25 <danielfb0554@gmail.com>
2025-11-12 00:02:47 -0600

    add form validation

--- scripts/index.js
+++ scripts/index.js
@@ -146,10 +146,7 @@ addCardForm.addEventListener("submit", handleCardFormSubmit);
 // M A N A G I N G   C A R D S
 
 // creates the card element
-function getCardElement(
-  name = "titulo no disponible",
-  link = "./images/placeholder.jpg"
-) {
+function getCardElement(name, link) {
   const cardElement = cardTemplate.cloneNode(true); // clones the template
   const cardTitle = cardElement.querySelector(".card__title"); // selects the title
   cardTitle.textContent = name; // gives a name to card title
@@ -200,11 +197,134 @@ function managePopupModal(cardTitle, cardImage) {
 
 // M A N A G E   M O D A L S
 
-// opens modal
+// Opens the modal (shows overlay + popup)
 function openModal(modal) {
-  modal.classList.add("popup_is-opened"); // no retrun statement as no value needs to be returned
+  modal.classList.add("popup_is-opened");
+  // Attach the overlay click listener to the modal itself
+  // This ensures clicks outside the popup trigger closing
+  modal.addEventListener("click", overLapClosesModal);
+  window.addEventListener("keydown", escKeyClosesModal);
 }
-// closes modal
+
+// Closes the modal (hides overlay + popup)
 function closeModal(modal) {
   modal.classList.remove("popup_is-opened");
+
+  // Remove the overlay click listener when closing
+  // This prevents multiple listeners from stacking each time it's opened
+  modal.removeEventListener("click", overLapClosesModal);
+  window.removeEventListener("keydown", escKeyClosesModal);
+  // resets validation once the modal is closed
+  resetForm(modal);
+}
+
+// Handles closing the modal when clicking on the overlay (background)
+function overLapClosesModal(evt) {
+  // evt.currentTarget → the element with the listener (the modal)
+  // evt.target → the exact element clicked (could be inside the popup)
+  const modal = evt.currentTarget;
+
+  // Close only if the user clicks directly on the overlay, not inside the popup
+  if (evt.target === modal) {
+    closeModal(modal);
+  }
+}
+
+function escKeyClosesModal(evt) {
+  if (evt.key === "Escape") {
+    // selects the opened modal and closes it
+    const openModal = document.querySelector(".popup_is-opened");
+    closeModal(openModal);
+  }
+}
+
+// ===========================
+//      FORM VALIDATION
+// ===========================
+
+// selecting all forms
+const forms = document.querySelectorAll("form");
+
+// show and hide error messages
+
+function showInputError(inputElement, errorMessage, form) {
+  // selecting the <span> tag associated with the input
+  // which will contain the error message
+  const errorSpan = form.querySelector(`.${inputElement.id}-input-error`);
+  // adding a red border to the input field.
+  inputElement.classList.add("form__input_type-error");
+  // adding the error message to the <span>
+  errorSpan.textContent = errorMessage;
+  errorSpan.classList.add("form__input-error-active");
+  // making it visible
+  errorSpan.classList.add("form__input-error");
+}
+
+function hideInputError(inputElement, form) {
+  // selecting the <span> tag associated with the input
+  // to remove the error message
+  const errorSpan = form.querySelector(`.${inputElement.id}-input-error`);
+  // removing the red border to the field
+  inputElement.classList.remove("form__input_type-error");
+  // removing the class that makes the span visible
+  errorSpan.classList.remove("form__input-error-active");
+  // removing the error message to make the span invisible again
+  errorSpan.textContent = "";
+}
+
+// enabling or disabling the submit button from validating each input
+
+function toggleButtonState(inputsList, button) {
+  // Convert the NodeList (inputs) into an array and check if *every* field is valid.
+  // every returns true if all inputs validity is valid
+  const allValid = Array.from(inputsList).every(
+    (input) => input.validity.valid
+  );
+  // Disable the button if not all inputs are valid, enable it otherwise.
+  button.disabled = !allValid;
+}
+
+// ============================================================
+// looping through forms to apply logic to each selected form
+// ============================================================
+
+forms.forEach((form) => {
+  // selecting form inputs
+  const inputs = form.querySelectorAll(".popup__input");
+  // console.log(inputs);
+
+  // selecting form submit button
+  const submitButton = form.querySelector(".popup__button-submit");
+  // console.log(submitButton.textContent);
+
+  // looping inputs and adding event listeners to validate fields as inputs occur
+  inputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      // if the input is not valid shows an error message
+      if (!input.validity.valid) {
+        showInputError(input, input.validationMessage, form);
+      } else {
+        hideInputError(input, form);
+      }
+      console.log(input.value);
+      console.log(input.validationMessage);
+      // After every change, re-check the whole form validity and update the button state
+      toggleButtonState(inputs, submitButton);
+    });
+  });
+});
+
+// resets validation
+function resetForm(formModal) {
+  // selecting items
+  const form = formModal.querySelector(".popup__form");
+  const elementsList = form.querySelectorAll(".popup__input");
+
+  //
+  elementsList.forEach((element) => {
+    // console.log(element);
+    hideInputError(element, form);
+  });
+  // reset form, important for add card modal
+  form.reset();
 }
